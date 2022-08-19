05ead9a079403584c355ae28b2d3ae98776dcaac
agbaye1 <59490337+agbaye1@users.noreply.github.com>
2022-08-18 22:34:55 -0500

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "mySonarInstance" {
-      ami           = "ami-0b9064170e32bde34"
+      ami           = "ami-052efd3df9dad4825"
 
       key_name = var.key_name
       instance_type = "t2.micro"
